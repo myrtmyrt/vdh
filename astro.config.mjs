38e81086d083cd0a9dfbb2d4e73611325f5ed9a6
pierre adorni <pierreadorni@free.fr>
2023-05-09 09:15:56 +0200

    image compression and various seo optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   site: 'https://assos.utc.fr/lavoieduhoublon',
-  base: '/lavoieduhoublon/'
+  base: '/lavoieduhoublon/',
 });
